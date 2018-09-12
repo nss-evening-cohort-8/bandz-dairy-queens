c61187c82fc3dbd2c8debe864535a1fe083447d2
Jasmine Walters <jsmnwltrs@gmail.com>
2018-09-12 18:35:22 -0500

    removed indention

--- main.js
+++ main.js
@@ -8,17 +8,17 @@ let songData = [
     songFour: "It's too Gouda to be true",
     songFive: 'You Feta believe it' }, 
     {albumName: 'Cows go moo',
-        songOne: 'I moo, therefore I am',
-        songTwo: 'Stinky fromage',
-        songThree: 'Udderly delicious',
-        songFour: 'Churn dat butta',
-        songFive: 'Thanks a latte' },
-        {albumName: 'Treat Your Beats',
-            songOne: 'Lick me till ice cream',
-            songTwo: 'Sundae school',
-            songThree: 'Whip my cream',
-            songFour: "Don't break my cheesecake",
-            songFive: 'Not pudding up with it' }
+    songOne: 'I moo, therefore I am',
+    songTwo: 'Stinky fromage',
+    songThree: 'Udderly delicious',
+    songFour: 'Churn dat butta',
+    songFive: 'Thanks a latte' },
+    {albumName: 'Treat Your Beats',
+    songOne: 'Lick me till ice cream',
+    songTwo: 'Sundae school',
+    songThree: 'Whip my cream',
+    songFour: "Don't break my cheesecake",
+    songFive: 'Not pudding up with it' }
 ];
 
 //Song Page Data End
