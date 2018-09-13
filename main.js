0fcd91635322f79049ef2470317b97403b2ff33b
Jasmine Walters <jsmnwltrs@gmail.com>
2018-09-12 19:13:50 -0500

    added print to dom and for loop functions for song page

--- main.js
+++ main.js
@@ -1,6 +1,14 @@
-//Song Page Data Start
+//Print To Dom function
 
-let songData = [
+const printToDom = (stringToPrint,divId) => {
+    const selectedDiv = document.getElementById(divId);
+    selectedDiv.innerHTML = stringToPrint;
+    
+};
+
+//Song Page Start
+
+const songData = [
     {albumName: 'Bring the Cheese',
     songOne: 'Born to Brie wild',
     songTwo: 'Munster Mash',
@@ -16,9 +24,26 @@ let songData = [
     {albumName: 'Treat Your Beats',
     songOne: 'Lick me till ice cream',
     songTwo: 'Sundae school',
-    songThree: 'Whip my cream',
+    songThree: 'Not pudding up with it',
     songFour: "Don't break my cheesecake",
-    songFive: 'Not pudding up with it' }
+    songFive: 'Whip my Cream' }
 ];
 
-//Song Page Data End
+const songLoop = () => {
+    let stringBuilder = '';
+    for (i = 0; i < songData.length; i++) {
+    stringBuilder += `<div class='albumSongs'>`; 
+    stringBuilder += `<h1>${songData[i].albumName}</h1>`;
+    stringBuilder += `<p>${songData[i].songOne}</p>`
+    stringBuilder += `<p>${songData[i].songTwo}</p>`
+    stringBuilder += `<p>${songData[i].songThree}</p>`
+    stringBuilder += `<p>${songData[i].songFour}</p>`
+    stringBuilder += `<p>${songData[i].songFive}</p>`
+    stringBuilder += `</div>`
+    };
+    printToDom(stringBuilder, 'allSongs')
+};
+
+songLoop();
+
+//Song Page End
