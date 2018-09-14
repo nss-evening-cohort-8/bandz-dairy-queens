b057ec7287e187cc1deb8239e85c7cbf97fe0c71
Ripal Patel <ripal212007@gmail.com>
2018-09-13 20:53:37 -0500

    completed funtion to slide images using settimeout method

--- main.js
+++ main.js
@@ -12,22 +12,21 @@ const printToDom = (stringToPrint, divId) => {
     const selectedDiv = document.getElementById(divId);
     selectedDiv.innerHTML = stringToPrint; 
 }
-
-const imagesStringBuilder =() => {
-   
-	for (i=0; i < images.length; i++){
-         let newString = `<img src="${images[i]}" width="1000px">`;
-         if(i < images.length){
-            i++;
-        }else {
-            i = 0;
-        }
-        let counter = 0;
-    setTimeout(() => {
-        counter++;
-        printToDom(newString, 'image_container')}, 3000); 
-}
-}
-imagesStringBuilder();
-
+//   function to slide Images
+const slideMyImages= (i) => {
+     setTimeout(()=> {    
+      newString=`<img src="${images[i]}" width= "900" height="500">`;
+       printToDom(newString, "image_container");
+    }, i * 3000);
+  }
+// function to loop through images
+const slideImage= () => {
+    for(i=0; i<images.length; i++){
+      slideMyImages(i);
+    }
+  }
+slideImage();
+setInterval(slideImage, 18000);
+  
 // image sliding ends
+
